6a551e3e8632f4b55145b7e789175ab73a4a4484
gbiamgaurav <gaurav.bhattacharya10@gmail.com>
2024-07-15 20:36:41 +0530

    env & folder structure created

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="precipitation",
+    version="0.0.0",
+    author="gbiamgaurav",
+    author_email="gaurav.bhattacharya10@gmail.com",
+    packages=find_packages()
+)
